3bfb986f1a6f93304ac462a38210a3e99ed55d2b
Cuboid <yyytaiwanzed@gmail.com>
2022-07-27 15:54:09 +0800

    first commit

---
+++ sanitybackend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "留言",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "Posted by",
+      type: "postedBy",
+    },
+    {
+      name: "comment",
+      title: "留言",
+      type: "string",
+    },
+  ],
+};
---
+++ sanitybackend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  title: "使用者",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      title: "使用者名稱",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "圖片",
+      type: "string",
+    },
+  ],
+};
